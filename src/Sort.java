9410fcb910a165d5cedf2537e33bb53f7e48a847
golsa444 <golsadorsamccoy@yahoo.com>
2021-11-13 13:07:09 +0100

    WIP...

--- src/Sort.java
+++ src/Sort.java
@@ -2,32 +2,6 @@ import javax.swing.*;
 import java.util.Arrays;
 
 public class Sort {
-    /* String num = JOptionPane.showInputDialog(null, "Enter your Numbers: ");
-    String[] strNum = num.split(",");*/
-    /*public static void  SortArray () {
-
-        int[] arr = new int[]{1, 4, 7, 8, 14, 10, 11, 43};
-        int tmp = 0;
-        System.out.println("Original Array: ");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i] + " ");
-        }
-        // sort array
-        for (int i : arr) {
-            for (int j=i + 1; j < arr.length; j++  ) {
-                if (arr[i] > arr[j]) {
-                    tmp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = tmp;
-                }
-            }
-        }
-        System.out.println("Sorted Array: ");
-        for(int i  = 0; i<arr.length; i++) {
-            System.out.println(arr[i] + " ");
-        }
-    }
-*/
 
     public static void SortArray() {
         // int [] arr = new int [] {52,45,-32,64,12,87,-78,98,23,7};
